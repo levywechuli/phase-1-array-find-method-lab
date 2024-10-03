0bc8c55e612229622fc0a118efaabb5cc68caa08
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-10-03 07:32:25 +0300

    find method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function superbowlWin(records) {
+    const winRecord = records.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+}
+
+
+const superBowls = [
+    { year: 2017, result: "L" },
+    { year: 2018, result: "W" },
+    { year: 2019, result: "L" }
+];
+
+console.log(superbowlWin(superBowls));
